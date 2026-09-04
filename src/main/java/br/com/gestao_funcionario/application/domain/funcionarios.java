ee83582540eada0927d4e02996bee8452b2d195a
Erivania Leal <erivanial.s1507@gmail.com>
2026-09-03 21:00:26 -0300

    Implementa domain

--- src/main/java/br/com/gestao_funcionario/application/domain/funcionarios.java
+++ src/main/java/br/com/gestao_funcionario/application/domain/funcionarios.java
@@ -1,4 +1,12 @@
 package br.com.gestao_funcionario.application.domain;
 
+import java.util.UUID;
+
 public class funcionarios {
+    private UUID id;
+    private String nome;
+    private String designacao;
+    private String salario;
+    private String telefone;
+    private String endereço;
 }
